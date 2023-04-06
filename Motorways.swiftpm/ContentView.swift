44a9acf9d587fb73b744fe5e0540b4e24d9f1a9c
Isabelle Colares da Silva <isabelle.colares.2@gmail.com>
2023-04-06 18:45:44 -0400

    Add UI Speed and logic print

--- Motorways.swiftpm/ContentView.swift
+++ Motorways.swiftpm/ContentView.swift
@@ -8,125 +8,186 @@
 import Foundation
 import SwiftUI
 
-struct Moto {
-    var name: String
-    var speed: String
-    var budget: String
-    var imageName: String
-    var isChosen: Bool = false
-}
-
-import SwiftUI
-
 struct ContentView: View {
     
-    let motorcycleOptions = ["Chopper", "Scooter", "Sport"]
-    
-    @State var selectedOptions: [String] = []
-    @State var gameResult: String? = nil
+    @StateObject var answer = Answers()
+
     
     var body: some View {
-        VStack {
-            // Cabeçalho com características do cliente
-            VStack(alignment: .leading) {
+        VStack{
+            ZStack{
+                Rectangle()
+                    .fill(Color.black) // Cor sólida
+                       .cornerRadius(10)
+                
+                HStack{
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .padding()
+                        
+                    Text ("I don't want to run too fast, it has to be safe and take me places in the best possible way.")
+                        .lineLimit(2)
+                        .truncationMode(.tail)
+                        .foregroundColor(.white)
+                        .font(.title2)
+                        .padding()
+                        
+                } .padding()
+            } .frame(width: 1100, height: 150)
+
+            
+            
+            Text ("Which of these has the ")
+                .foregroundColor(.black)
+                .font(.largeTitle)
+            + Text("speed").bold()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+            + Text(" the customer wants?")
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                        
+            HStack (spacing: 100){
+                
+               
+                VStack {
+                    Image(systemName: "person.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 250)
+                                .padding()
+                    
+                            Text("Sooter")
+                                .font(.title)
+                                .foregroundColor(.primary)
+                                .padding()
+                            Text("Slow")
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                                .padding()
+                        }
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .onTapGesture {
+                            // Ação a ser executada quando o card for clicado
+                            answer.Answers[0] = "Sooter"
+                        }
+                
+                VStack {
+                    Image(systemName: "person.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 250)
+                                .padding()
+                    
+                            Text("Chopper")
+                                .font(.title)
+                                .foregroundColor(.primary)
+                                .padding()
+                            Text("Medium")
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                                .padding()
+                        }
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .onTapGesture {
+                            // Ação a ser executada quando o card for clicado
+                            answer.Answers[0] = "Chopper"
+                        }
+                
+                VStack {
+                    Image(systemName: "person.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 250)
+                                .padding()
+                    
+                            Text("Sport")
+                                .font(.title)
+                                .foregroundColor(.primary)
+                                .padding()
+                            Text("Fast")
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                                .padding()
+                        }
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .onTapGesture {
+                            // Ação a ser executada quando o card for clicado
+                            answer.Answers[0] = "Sport"
+                        }
+            
+            } 
+            
+            Spacer()
+            
+            HStack (spacing: 300){
+                
                 HStack {
-                    Image(systemName: "person.fill")
-                    Text("Cliente: Moderado, 2 pessoas, médio ou baixo")
+                    Image(systemName: "person.crop.artframe")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.black)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person.crop.artframe")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.black)
+                        .padding()
+                    
+                    Spacer()
+
+                    
+                    Image(systemName: "person.crop.artframe")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.black)
+                        .padding()
+                    
+                    
+                    
+                } .frame(width: 500, height: 150)
+                
+                
+                
+                Button(action: {
+                    // Adicione sua ação aqui
+                    print(answer.Answers[0])
+                }) {
+                    Text("Confirm")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
                 }
-                Spacer()
-            }
-            .padding()
-            
-            // Opções de moto
-            HStack {
-                ForEach(motorcycleOptions, id: \.self) { option in
-                    MotorcycleOptionCard(option: option, selectedOptions: $selectedOptions)
+                
+              
+                    
                 }
+                
+                
             }
             
-            // Botão de confirmação
-            Button(action: {
-                gameResult = checkAnswer(chosenAnswers: selectedOptions)
-            }, label: {
-                Text("Confirmar escolhas")
-                    .foregroundColor(.white)
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 20)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            })
-            .padding()
-            
-            // Mensagem de resultado
-            if let result = gameResult {
-                Text(result)
-                    .padding()
-            }
-            
-            Spacer()
         }
-    }
-}
-
-struct MotorcycleOptionCard: View {
-    
-    let option: String
-    @Binding var selectedOptions: [String]
-    
-    var body: some View {
-        VStack {
-            Image(systemName: "motorcycle")
-            Text(option)
-                .padding(.vertical, 5)
-            Text("Características")
-                .font(.caption)
-            Spacer()
-        }
-        .padding(10)
-        .background(selectedOptions.contains(option) ? Color.blue.opacity(0.2) : Color.white)
-        .cornerRadius(10)
-        .onTapGesture {
-            if let index = selectedOptions.firstIndex(of: option) {
-                selectedOptions.remove(at: index)
-            } else {
-                selectedOptions.append(option)
-            }
-        }
-    }
-}
-
-
-func checkAnswer(chosenAnswers: [String]) -> String {
-    var chopperCount = 0
-    var scooterCount = 0
-    var sportCount = 0
-    
-    for answer in chosenAnswers {
-        switch answer {
-        case "Chopper":
-            chopperCount += 1
-        case "Scooter":
-            scooterCount += 1
-        case "Sport":
-            sportCount += 1
-        default:
-            break
-        }
+        
     }
     
-    if chopperCount >= 2 {
-        return "Resposta certa reflexiva - Chopper"
-    } else if scooterCount >= 2 {
-        return "Resposta reflexiva - Scooter"
-    } else if sportCount >= 2 {
-        return "Resposta errada motivacional - Sport"
-    } else {
-        return "Resposta errada motivacional"
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+        
+        
