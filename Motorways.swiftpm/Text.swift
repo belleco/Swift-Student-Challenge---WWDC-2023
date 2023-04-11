3f601e8686d6962aa2dea0fedfed96e09f45dd6c
Isabelle Colares da Silva <isabelle.colares.2@gmail.com>
2023-04-11 19:48:44 -0400

    Fix array text in Text View

--- Motorways.swiftpm/Text.swift
+++ Motorways.swiftpm/Text.swift
@@ -30,15 +30,15 @@ var showQuestionView: [[ListQuestionView]] = [
 [
     //Primeiro Card = Scooter
 
-    ListQuestionView(imageClient: "person.circle", conversation: "I don't want to run too fast, it has to be safe and take me places in the best possible way.", firstWords: "Which of these has the ", boldWord: "speed", lastWord: "the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Scooter", subtitle: "Slow", information: "It has a maximum speed of around 50 km/h and its autonomy range can vary from 50 km to 120 km, depending on the model and conditions of use."),
+    ListQuestionView(imageClient: "person.circle", conversation: "I don't want to run too fast, it has to be safe and take me places in the best possible way.", firstWords: "Which of these has the ", boldWord: "speed", lastWord: " the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Scooter", subtitle: "Slow", information: "It has a maximum speed of around 50 km/h and its autonomy range can vary from 50 km to 120 km, depending on the model and conditions of use."),
  
     //Segundo Card = Chopper
 
-    ListQuestionView(imageClient: "person.circle", conversation: "I don't want to run too fast, it has to be safe and take me places in the best possible way.", firstWords: "Which of these has the ", boldWord: "speed", lastWord: "the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Chopper", subtitle: "Medium", information: "texto aqui 2"),
+    ListQuestionView(imageClient: "person.circle", conversation: "I don't want to run too fast, it has to be safe and take me places in the best possible way.", firstWords: "Which of these has the ", boldWord: "speed", lastWord: " the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Chopper", subtitle: "Medium", information: "texto aqui 2"),
 
     //Terceiro Card = Sport
 
-    ListQuestionView(imageClient: "person.circle", conversation: "I don't want to run too fast, it has to be safe and take me places in the best possible way.", firstWords: "Which of these has the ", boldWord: "speed", lastWord: "the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Sport", subtitle: "Fast", information: "texto aqui 3")
+    ListQuestionView(imageClient: "person.circle", conversation: "I don't want to run too fast, it has to be safe and take me places in the best possible way.", firstWords: "Which of these has the ", boldWord: "speed", lastWord: " the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Sport", subtitle: "Fast", information: "texto aqui 3")
 ],
 
 
@@ -52,7 +52,7 @@ var showQuestionView: [[ListQuestionView]] = [
                      conversation: "I'd like to be able to give a ride to a friend when they need it... But it isn't something that impacts my routine.",
                      firstWords: "Which of these has the ",
                      boldWord: "capacity",
-                     lastWord: "the customer wants?",
+                     lastWord: " the customer wants?",
                      imageMotor: "photo",
                      icon: "info.circle",
                      title: "Scooter",
@@ -65,7 +65,7 @@ var showQuestionView: [[ListQuestionView]] = [
                      conversation: "I'd like to be able to give a ride to a friend when they need it... But it isn't something that impacts my routine.",
                      firstWords: "Which of these has the ",
                      boldWord: "capacity",
-                     lastWord: "the customer wants?",
+                     lastWord: " the customer wants?",
                      imageMotor: "photo",
                      icon: "info.circle",
                      title: "Chopper",
@@ -78,7 +78,7 @@ var showQuestionView: [[ListQuestionView]] = [
                      conversation: "I'd like to be able to give a ride to a friend when they need it... But it isn't something that impacts my routine.",
                      firstWords: "Which of these has the ",
                      boldWord: "capacity",
-                     lastWord: "the customer wants?",
+                     lastWord: " the customer wants?",
                      imageMotor: "photo",
                      icon: "info.circle",
                      title: "Sport",
@@ -93,15 +93,15 @@ var showQuestionView: [[ListQuestionView]] = [
 [
     //Primeiro Card = Scooter
 
-    ListQuestionView(imageClient: "person.circle", conversation: "Little by little I've been saving my money from my college project... It's not that much but my dad said he'd help me with something if I needed it.", firstWords: "Which of these has the ", boldWord: "budget", lastWord: "the customer has?", imageMotor: "photo", icon: "info.circle", title: "Scooter", subtitle: "Low", information: "texto aqui 1"),
+    ListQuestionView(imageClient: "person.circle", conversation: "Little by little I've been saving my money from my college project... It's not that much but my dad said he'd help me with something if I needed it.", firstWords: "Which of these has the ", boldWord: "budget", lastWord: " the customer has?", imageMotor: "photo", icon: "info.circle", title: "Scooter", subtitle: "Low", information: "texto aqui 1"),
 
     //Segundo Card = Chopper
 
-    ListQuestionView(imageClient: "person.circle", conversation: "Little by little I've been saving my money from my college project... It's not that much but my dad said he'd help me with something if I needed it.", firstWords: "Which of these has the ", boldWord: "budget", lastWord: "the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Chopper", subtitle: "Low and Medium", information: "texto aqui 2"),
+    ListQuestionView(imageClient: "person.circle", conversation: "Little by little I've been saving my money from my college project... It's not that much but my dad said he'd help me with something if I needed it.", firstWords: "Which of these has the ", boldWord: "budget", lastWord: " the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Chopper", subtitle: "Low and Medium", information: "texto aqui 2"),
 
     //Terceiro Card = Sport
 
-    ListQuestionView(imageClient: "person.circle", conversation: "Little by little I've been saving my money from my college project... It's not that much but my dad said he'd help me with something if I needed it.", firstWords: "Which of these has the ", boldWord: "budget", lastWord: "the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Sport", subtitle: "High", information: "texto aqui 3")
+    ListQuestionView(imageClient: "person.circle", conversation: "Little by little I've been saving my money from my college project... It's not that much but my dad said he'd help me with something if I needed it.", firstWords: "Which of these has the ", boldWord: "budget", lastWord: " the customer wants?", imageMotor: "photo", icon: "info.circle", title: "Sport", subtitle: "High", information: "texto aqui 3")
 ]
 ]
 
